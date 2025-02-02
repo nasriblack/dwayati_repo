f4d3d6f2cfc1c779abf05804fbea900e50ff776e
Turbobot <turbobot@vercel.com>
2025-02-02 22:38:46 +0100

    feat(create-turbo): apply official-starter transform

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,37 +1,20 @@
-import * as React from "react";
-import {
-  TouchableOpacity,
-  StyleSheet,
-  GestureResponderEvent,
-  Text,
-} from "react-native";
+"use client";
 
-export interface ButtonProps {
-  text: string;
-  onClick?: (event: GestureResponderEvent) => void;
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  className?: string;
+  appName: string;
 }
 
-export function Button({ text, onClick }: ButtonProps) {
+export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
-    <TouchableOpacity style={styles.button} onPress={onClick}>
-      <Text style={styles.text}>{text}</Text>
-    </TouchableOpacity>
+    <button
+      className={className}
+      onClick={() => alert(`Hello from your ${appName} app!`)}
+    >
+      {children}
+    </button>
   );
-}
-
-const styles = StyleSheet.create({
-  button: {
-    maxWidth: 200,
-    textAlign: "center",
-    borderRadius: 10,
-    paddingTop: 14,
-    paddingBottom: 14,
-    paddingLeft: 30,
-    paddingRight: 30,
-    fontSize: 15,
-    backgroundColor: "#2f80ed",
-  },
-  text: {
-    color: "white",
-  },
-});
+};
