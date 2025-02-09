74e80e4a8d1a253d4318734356402f0203d442ad
Nasereddine Lakhal <naserlakhal@gmail.com>
2025-02-09 20:48:26 +0100

    every think is working , shared devDependecies

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,6 @@
 import { createServer } from "./server";
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3005;
 const server = createServer();
 
 server.listen(port, () => {
