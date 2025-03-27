5f58f5069e6b4beafded47551c25c7462f766a2c
Nasereddine Lakhal <naserlakhal@gmail.com>
2025-03-28 00:46:23 +0100

    implement the get and post test of prescription

--- apps/api/__tests__/services/prescriptions.test.ts
+++ apps/api/__tests__/services/prescriptions.test.ts
@@ -4,12 +4,25 @@ import { createServer } from "../../src/server";
 import { apiVersion, endPoint } from "../../src/utils/endPoint";
 import HttpStatusCode from "../../src/utils/HttpStatusCode";
 
+const payload = {
+  doctorName: "Dr. Test",
+  description: "This is a test",
+  medication: [],
+};
+
+const checkPrescriptionProperty = (prescription: any) => {
+  expect(prescription).toHaveProperty("id");
+  expect(prescription).toHaveProperty("createdAt");
+  expect(prescription).toHaveProperty("description");
+  expect(prescription).toHaveProperty("doctorName");
+  expect(prescription).toHaveProperty("medications");
+};
+
 describe("Check Prescription Service", () => {
   it("should return success status code", async () => {
     const response = await request(createServer()).get(
       `${apiVersion}${endPoint.prescriptionEndPoint.ALL_PRESCRIPTION}`
     );
-    console.log("checking the response", response);
     expect(response.status).toBe(HttpStatusCode.OK);
   });
   it("should return not found status code", async () => {
@@ -17,5 +30,27 @@ describe("Check Prescription Service", () => {
       `${apiVersion}${endPoint.prescriptionEndPoint.ALL_PRESCRIPTION}unkown`
     );
     expect(response.status).toBe(HttpStatusCode.NOT_FOUND);
+  });
+
+  it("should return the right schema for the response", async () => {
+    const response = await request(createServer()).get(
+      `${apiVersion}${endPoint.prescriptionEndPoint.ALL_PRESCRIPTION}`
+    );
+    const bodyResponse: [] = response?.body?.data;
+    expect(response.status).toBe(HttpStatusCode.OK);
+    expect(Array.isArray(bodyResponse)).toBe(true);
+    bodyResponse.forEach((prescription: any) => {
+      checkPrescriptionProperty(prescription);
+    });
+  });
+
+  it("should create a prescription and return status 201", async () => {
+    const response = await request(createServer())
+      .post(`${apiVersion}${endPoint.prescriptionEndPoint.CREATE_PRESCRIPTION}`)
+      .send(payload)
+      .expect(201);
+    const responseBody = response?.body;
+    expect(responseBody.success).toBe(true);
+    checkPrescriptionProperty(responseBody.data);
   });
 });
