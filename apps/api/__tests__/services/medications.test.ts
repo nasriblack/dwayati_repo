c50974c1b0d86f45a5187f54f1206fb78b5589fa
Nasereddine Lakhal <naserlakhal@gmail.com>
2025-03-29 13:07:22 +0100

    medication unit test for schema response

--- apps/api/__tests__/services/medications.test.ts
+++ apps/api/__tests__/services/medications.test.ts
@@ -4,6 +4,15 @@ import { createServer } from "../../src/server";
 import { apiVersion, endPoint } from "../../src/utils/endPoint";
 import HttpStatusCode from "../../src/utils/HttpStatusCode";
 
+const checkMedicationProperty = (medication: any) => {
+  expect(medication).toHaveProperty("id");
+  expect(medication).toHaveProperty("name");
+  expect(medication).toHaveProperty("description");
+  expect(medication).toHaveProperty("expirationDate");
+  expect(medication).toHaveProperty("tag");
+  expect(medication).toHaveProperty("prescription");
+};
+
 describe("Check Medications Service", () => {
   it("should return success status code", async () => {
     const response = await request(createServer()).get(
@@ -16,5 +25,16 @@ describe("Check Medications Service", () => {
       `${apiVersion}${endPoint.prescriptionEndPoint.ALL_PRESCRIPTION}unkown`
     );
     expect(response.status).toBe(HttpStatusCode.NOT_FOUND);
+  });
+  it("should return the right schema for medication response", async () => {
+    const response = await request(createServer()).get(
+      `${apiVersion}${endPoint.medicationEndPoint.ALL_MEDICATIONS}`
+    );
+    const bodyResponse: [] = response?.body?.data;
+    expect(response.status).toBe(HttpStatusCode.OK);
+    expect(Array.isArray(bodyResponse)).toBe(true);
+    bodyResponse.forEach((medication: any) => {
+      checkMedicationProperty(medication);
+    });
   });
 });
